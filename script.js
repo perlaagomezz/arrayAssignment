966f8f84f78d16568c42cad6258943a3178596c9
perlaagomezz <gomezperla879@gmail.com>
2022-01-26 10:09:19 -0800

    added

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+document.write("<h1>ARRAYS</h1>")
+
+const colors =["blue" , "red" , "pink" , "yellow" , "brown"]
+
+console.log(colors[3]);
+
+colors.pop('blue')
+
+const towns =["Prineville" , "Bend" , "Redmond" , "Madras" , "Culver"]
+
+console.log( towns[2]);
+
+towns.reverse('Redmond')
